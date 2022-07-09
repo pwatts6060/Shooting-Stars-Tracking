6799e28903c8f5018d2cb4ed7001e5aa14ae212d
Patrick <patrick.watts@hotmail.co.uk>
2022-07-10 00:48:23 +0400

    Remove unnecessary variable that can be inlined

--- src/main/java/com/shootingstartracking/ShootingStarTrackingConfig.java
+++ src/main/java/com/shootingstartracking/ShootingStarTrackingConfig.java
@@ -7,19 +7,16 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("Shooting Stars Tracking")
 public interface ShootingStarTrackingConfig extends Config
 {
-    int TIME_TILL_REMOVE = 85;
-    boolean DISPLAY_AS_MINUTES = false;
-
     @ConfigItem(
             keyName = "timeTillRemove",
             name = "Minutes till stars are removed",
             description = "The number of minutes after landing the star will remain on the list")
-    default int timeTillRemoveConfig() {return TIME_TILL_REMOVE;}
+    default int timeTillRemoveConfig() {return 85;}
 
     @ConfigItem(
             keyName = "displayAsTime",
             name = "Show minutes remaining",
             description = "Show the minutes remaining rather than the expected time"
     )
-    default boolean displayAsMinutes() {return DISPLAY_AS_MINUTES;}
+    default boolean displayAsMinutes() {return false;}
 }
